704b4bc41d603a606b436c3a5b60151166793244
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-31 08:43:47 +0900

    create hello world by express

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const PORT = process.env.PORT;
-console.log(`Application listening at ${PORT}`);
+const express = require("express");
+const app = express();
 
-if (true){
-  console.log("ERROR");
-}
+app.get("/", (req, res) => {
+  res.end("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Application listening at ${PORT}`);
+});
