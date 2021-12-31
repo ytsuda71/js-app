cb2d21688481dfce8a41a74cfc381e6788c9eb75
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-31 23:34:13 +0900

    Deliver static contents

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const PORT = process.env.PORT;
+const path = require("path");
 const express = require("express");
 const app = express();
 
+// Express Settings
 app.set("view engine", "ejs");
+
+// Static resource routing
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/index.js"));
 
+//Execute web application
 app.listen(PORT, () => {
   console.log(`Application listening at ${PORT}`);
 });
