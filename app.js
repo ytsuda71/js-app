affe93781c103d0c705361e569088bb4fc014b8f
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-01-01 00:40:45 +0900

    add favicon

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const PORT = process.env.PORT;
 const path = require("path");
 const express = require("express");
+const favicon = require("serve-favicon");
 const app = express();
 
 // Express Settings
 app.set("view engine", "ejs");
 
 // Static resource routing
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/index.js"));
