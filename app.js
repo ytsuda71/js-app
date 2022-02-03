83d94b1e3e5652f095bb10a582c87fe42d5c7028
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-02-03 22:46:45 +0900

    crate application log output

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT;
 const path = require("path");
 const logger = require("./lib/log/logger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
@@ -15,7 +16,10 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/index.js"));
 
+// Set application log.
+app.use(applicationlogger());
+
 //Execute web application
 app.listen(PORT, () => {
-  logger.console.info(`Application listening at ${PORT}`);
+  logger.application.info(`Application listening at ${PORT}`);
 });
--- config/log4js.config.js
+++ config/log4js.config.js
@@ -1,13 +1,29 @@
+const path = require("path");
+const LOG_ROOT_DIR = process.env.LOG_ROOT_DIR || path.join(__dirname, "../logs");
+
 module.exports = {
   appenders: {
     ConsoleLogAppender: {
       type: "console"
+    },
+    ApplicationLogAppender: {
+      type: "dateFile",
+      filename: path.join(LOG_ROOT_DIR, "./application.log"),
+      pattern: "yyyyMMdd",
+      numBackups: 7
     }
   },
   categories: {
     "default": {
       appenders: ["ConsoleLogAppender"],
       level: "ALL"
+    },
+    "application": {
+      appenders: [
+        "ApplicationLogAppender",
+        "ConsoleLogAppender"
+      ],
+      level: "INFO"
     }
   }
 };
